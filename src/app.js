1757eb388693a7a0830e40d094db70d7547d140d
RodrigoLau777 <darkbass16@gmail.com>
2024-09-20 17:23:26 +0000

    proyectoJS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //Función para crear elemento aleatorio array
+
+  function getRandomElement(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  // Generar la excusa
+
+  function generateExcuse() {
+    let excuse =
+      getRandomElement(who) +
+      " " +
+      getRandomElement(action) +
+      " " +
+      getRandomElement(what) +
+      " " +
+      getRandomElement(when) +
+      ".";
+
+    return excuse;
+  }
+
+  // Mostrar la excusa en el HTML
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
